10b9cbdf971c84cd70dd930ac639d6cf29d28e0a
Kurt Andres <kurt.andres@my.wheaton.edu>
2014-04-14 22:41:59 -0500

    Update addTimeline.php

--- addTimeline.php
+++ addTimeline.php
@@ -11,11 +11,31 @@ $name = "'".$_GET['name']."'";
 //echo $name;
 
 if($uid!= "''" | $name != "''"){
-	$sql="INSERT INTO timelines(uid, name) VALUES (".$uid.",".$name.")";
-	mysqli_query($db, $sql);
-	echo "added";
-	}else {
-		echo "Not valid php entry";
+	$result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
+	$row = mysqli_fetch_array($result);
+	if("'".$row['name']."'" == $name){
+		//echo "Duplicate Timeline";		
+		}else{
+			$sql="INSERT INTO timelines(uid, name) VALUES (".$uid.",".$name.")";
+			mysqli_query($db, $sql);
+			//echo "added";			
+			}
 		}
+	
+if($uid!= "''"){
+	$result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
+	$row = mysqli_fetch_array($result);
+	echo json_encode($row);	
+	//$row = mysqli_fetch_array($result);
+	//$tid = $row[tid];
+	//echo $tid;
+$sql="INSERT INTO userAdmin(name, password) VALUES (".$name.",".$password.")";
+		mysqli_query($db, $sql);
+		$result = mysqli_query($db, "SELECT * FROM userAdmin WHERE name=".$name);
+		$row = mysqli_fetch_array($result);
+		$uid = $row[uid];
+		echo $uid;	
+	}
 mysqli_close($db);
+
 ?>
