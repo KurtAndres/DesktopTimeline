e8d2c51eb9d04bc8541efb6f2dfc5e06970fa7b1
Kurt Andres <kurt.andres@my.wheaton.edu>
2014-04-21 19:40:58 -0500

    Support for background, axis and labels added
    Added support to store axis labels, background color, and axis color in the database and return these in the json encoded output

--- addTimeline.php
+++ addTimeline.php
@@ -1,3 +1,4 @@
+
 <?php
 //connecting to the db
 $db = mysqli_connect('csdb.wheaton.edu', 'TimelineAdmin', 'kurtconner',"Timeline");
@@ -10,38 +11,47 @@ $uid = "'".$_GET['uid']."'";
 $name = "'".$_GET['name']."'";
 //echo $name;
 
-if($uid!= "''" & $name != "''"){
-        $result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
-        $found = false;
-        while($row = mysqli_fetch_assoc($result)){
-                if("'".$row['name']."'" == $name){
-                //echo "Duplicate Timeline";
-                $found = true;
-                 }else{
-                        //echo " Not Duplicate ";
-                        }
-        }
-if($found){
-                //echo "Not Added";
-                }else{
-                        $sql="INSERT INTO timelines(uid, name) VALUES (".$uid.",".$name.")";
-                        mysqli_query($db, $sql);
-                        //echo "added";
-                        }
-                }
+//get axis_label
+$axis_label = "'".$_GET['axis_label']."'";
 
+//get axis_color
+$axis_color = "'".$_GET['axis_color']."'";
+
+//get background color
+$background_color = "'".$_GET['background_color']."'";
+
+if($uid!= "''" & $name != "''"){
+	$result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
+	$found = false;
+	while($row = mysqli_fetch_assoc($result)){
+		if("'".$row['name']."'" == $name){
+                //echo "Duplicate Timeline";
+               	$found = true;
+		 }else{
+			//echo " Not Duplicate ";
+			}
+	}
+	if($found){
+		//echo "Not Added";		
+		}else{
+			$sql="INSERT INTO timelines(uid, name, axis_label, background_color, axis_color) VALUES (".$uid.",".$name.",".$axis_label.",".$background_color.",".$axis_color.")";
+			mysqli_query($db, $sql);
+			//echo "added";			
+			}
+		}
+	
 if($uid!= "''"){
-        $result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
-        while($row = mysqli_fetch_assoc($result)){
-                $json[] = $row;
-                }
-        echo json_encode($json);
-        //$row = mysqli_fetch_assoc($result);
-        //echo json_encode($row);
-        //$row = mysqli_fetch_array($result);
-        //$tid = $row[tid];
-        //echo $tid;
-        }
+	$result = mysqli_query($db, "SELECT * FROM timelines WHERE uid=".$uid);
+	while($row = mysqli_fetch_assoc($result)){
+     		$json[] = $row;
+		}
+	echo json_encode($json);
+	//$row = mysqli_fetch_assoc($result);
+	//echo json_encode($row);	
+	//$row = mysqli_fetch_array($result);
+	//$tid = $row[tid];
+	//echo $tid;
+	}
 mysqli_close($db);
 
 ?>
