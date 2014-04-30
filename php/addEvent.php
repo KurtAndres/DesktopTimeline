a702d4d8dbb482362f5f805aec9d2dd126508c4a
Kurt Andres <kurt.andres@my.wheaton.edu>
2014-04-30 17:47:42 -0500

    Removed DB info to make public

--- php/addEvent.php
+++ php/addEvent.php
@@ -1,6 +1,6 @@
 <?php
 //connecting to the db
-$db = mysqli_connect('csdb.wheaton.edu', 'TimelineAdmin', 'kurtconner',"Timeline");
+$db = mysqli_connect('DB-LOCATION', 'DB-UserName', 'DB-Password',"DB-TableName");
 
 //eid - event id
 //if we are passed an eid this event already occurs so we are just updating it
